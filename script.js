e3b103f5bd37f5725b6425196121c56f59cecef3
Amy <86963506+aeposten@users.noreply.github.com>
2023-01-10 19:13:12 -0800

    can render selected number of colors

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const colorSelector = document.getElementById("color-input");
-let selectedColor = "";
+const schemeSelector = document.getElementById("scheme-select");
+const numSelector = document.getElementById("num-select");
+const colorSchemeEl = document.getElementById("color-scheme");
+
+let selectedColor = "FFDB58";
+let selectedScheme = "monochrome";
+let selectedNumber = 5;
+
+let colorsArray = [];
 
 function setSelectedColor() {
   selectedColor = colorSelector.value.slice(1);
@@ -7,13 +15,53 @@ function setSelectedColor() {
   return selectedColor;
 }
 
-colorSelector.addEventListener("input", () => {
-  setSelectedColor();
-  fetchColorScheme(selectedColor);
+function setSelectedScheme() {
+  selectedScheme = schemeSelector.value;
+
+  return selectedScheme;
+}
+
+function setSelectedNumber() {
+  selectedNumber = numSelector.value;
+  
+  return selectedNumber;
+}
+
+schemeSelector.addEventListener("change", () => {
+  setSelectedScheme();
+  fetchColorScheme(selectedColor, selectedScheme, selectedNumber);
 });
 
-function fetchColorScheme(colorValue) {
-  fetch(`https://www.thecolorapi.com/id?hex=${colorValue}`)
+colorSelector.addEventListener("input", () => {
+  setSelectedColor();
+  fetchColorScheme(selectedColor, selectedScheme, selectedNumber);
+});
+
+numSelector.addEventListener("change", () => {
+  setSelectedNumber();
+  fetchColorScheme(selectedColor, selectedScheme, selectedNumber);
+});
+
+function fetchColorScheme(colorValue, scheme, count) {
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${colorValue}&mode=${scheme}&count=${count}`
+  )
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => renderColors(data.colors));
 }
+
+function renderColors(colors) {
+  colorSchemeEl.innerHTML = colors
+    .map(
+      ({ hex, name }) => `
+    <section class="color">
+    <div class="color-bar" style="background-color:${hex.value}"></div>
+    <div class="color-name"></div>
+    <div class="hex">${hex.value}</div>
+    </section>
+    `
+    )
+    .join("");
+}
+
+fetchColorScheme(selectedColor, selectedScheme, selectedNumber);
