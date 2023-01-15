8fce87c4e8d6bb3866187bad1389c352a6637727
Amy <86963506+aeposten@users.noreply.github.com>
2023-01-14 18:11:13 -0800

    Reorganized JS, added additional styling for mobile devices, labeled form inputs

--- script.js
+++ script.js
@@ -15,6 +15,15 @@ let selectedColor = "FFDB58";
 let selectedScheme = "monochrome";
 let selectedNumber = 5;
 
+// Fetches default color scheme on page load
+fetchColorScheme(selectedColor, selectedScheme, selectedNumber);
+
+// Sets specified user actions using functions above then uses fetch function to getcolor data from API
+function setEventAction(action) {
+  action();
+  fetchColorScheme(selectedColor, selectedScheme, selectedNumber);
+}
+
 // Event listeners for user selections
 schemeSelector.addEventListener("change", () => {
   setEventAction(setSelectedScheme);
@@ -26,30 +35,6 @@ colorSelector.addEventListener("input", () => {
 
 numSelector.addEventListener("change", () => {
   setEventAction(setSelectedNumber);
-});
-
-document.addEventListener("click", function (e) {
-  // Event to copy  color's hex value to clipboard
-  if (e.target.dataset.hex) {
-    copyHex(e.target, hexCopiedEl);
-  }
-
-  // Event to copy color's hex value from a saved color scheme
-  if (e.target.dataset.hexsm) {
-    copyHex(
-      e.target,
-      e.target.parentElement.parentElement.parentElement.children.namedItem(
-        "hex"
-      )
-    );
-  }
-
-  // Deletes selected color scheme from local storage and renders saved schemes
-  if (e.target.dataset.delete) {
-    deleteSavedScheme(e.target.dataset.delete);
-    retrievedColors = JSON.parse(localStorage.getItem("savedSchemes"));
-    renderSavedSchemes(retrievedColors);
-  }
 });
 
 // Sets color selected by user
@@ -73,6 +58,82 @@ function setSelectedNumber() {
   return selectedNumber;
 }
 
+// Fetches color scheme from colors api based on user selections
+function fetchColorScheme(colorValue, scheme, count) {
+  try {
+    fetch(
+      `https://www.thecolorapi.com/scheme?hex=${colorValue}&mode=${scheme}&count=${count}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setColorScheme(data.colors);
+        renderColors(data.colors);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Renders colors on page
+function renderColors(colors) {
+  colorSchemeEl.innerHTML = colors
+    .map(
+      ({ hex, name }, index) => `
+    <section class="color">
+    <div class="color-bar" style="background-color:${hex.value}"></div>
+    <div class="hex" data-hex="${index}">${hex.value}</div>
+    </section>
+    `
+    )
+    .join("");
+
+  // If saved color schemes are found in local storage they are rendered on the page
+  if (localStorage.getItem("savedSchemes")) {
+    retrievedColors = JSON.parse(localStorage.getItem("savedSchemes"));
+    renderSavedSchemes(retrievedColors);
+  }
+}
+
+// Copies hex to clipboard
+function copyHex(element, messageEl) {
+  let copiedHex = element.textContent;
+
+  try {
+    navigator.clipboard.writeText(copiedHex).then(() => {
+      messageEl.textContent = `${copiedHex} Copied to Clipboard`;
+      setTimeout(() => {
+        messageEl.textContent = "";
+      }, 3000);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+document.addEventListener("click", function (e) {
+  // Event to copy  color's hex value to clipboard
+  if (e.target.dataset.hex) {
+    copyHex(e.target, hexCopiedEl);
+  }
+
+  // Event to copy color's hex value from a saved color scheme
+  if (e.target.dataset.hexsm) {
+    copyHex(
+      e.target,
+      e.target.parentElement.parentElement.parentElement.children.namedItem(
+        "hex"
+      )
+    );
+  }
+
+  // Deletes selected color scheme from local storage and re-renders saved schemes
+  if (e.target.dataset.delete) {
+    deleteSavedScheme(e.target.dataset.delete);
+    retrievedColors = JSON.parse(localStorage.getItem("savedSchemes"));
+    renderSavedSchemes(retrievedColors);
+  }
+});
+
 // Saves selected color scheme to local storage and then renders saved schemes
 saveBtn.addEventListener("click", () => {
   saveColorScheme(colorScheme);
@@ -86,28 +147,22 @@ function setColorScheme(colors) {
 
 // For use in save button event listener, adds additional color schemes to local storage
 function saveColorScheme(scheme) {
-  // If colors are saved in local storgae, retrieve them and use them to set saved colors variable
   if (localStorage.getItem("savedSchemes")) {
     savedColors = JSON.parse(localStorage.getItem("savedSchemes"));
   }
 
-  // Add desired color scheme to saved colors array, then ave the array in local storage
   savedColors.push(scheme);
   localStorage.setItem("savedSchemes", JSON.stringify(savedColors));
 
-  // Get saved color schemes from local storage, parse the JSON and save the parsed array in retrievedColors
   retrievedColors = JSON.parse(localStorage.getItem("savedSchemes"));
 }
 
 // For use in delete button event listener, deletes color schemes from local storage
 function deleteSavedScheme(schemeIndex) {
-  // Retrieves saved colorschemes from local storage, parses JSON and stores array in savedColors
   savedColors = JSON.parse(localStorage.getItem("savedSchemes"));
 
-  // Removes scheme from savedColorsarray
   savedColors.splice(schemeIndex, 1);
 
-  // stores edited savedColors array to local storage
   localStorage.setItem("savedSchemes", JSON.stringify(savedColors));
 }
 
@@ -149,64 +204,3 @@ function renderSavedSchemes(schemes) {
     </li>
   `;
 }
-
-// Sets specified user actions using functions above then uses fetch function to getcolor data from API
-function setEventAction(action) {
-  action();
-  fetchColorScheme(selectedColor, selectedScheme, selectedNumber);
-}
-
-// Copies hex to clipboard
-function copyHex(element, messageEl) {
-  let copiedHex = element.textContent;
-
-  try {
-    navigator.clipboard.writeText(copiedHex).then(() => {
-      messageEl.textContent = `${copiedHex} Copied to Clipboard`;
-      setTimeout(() => {
-        messageEl.textContent = "";
-      }, 3000);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-// Fetches color scheme from colors api based on user selections
-function fetchColorScheme(colorValue, scheme, count) {
-  try {
-    fetch(
-      `https://www.thecolorapi.com/scheme?hex=${colorValue}&mode=${scheme}&count=${count}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setColorScheme(data.colors);
-        renderColors(data.colors);
-      });
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-// Renders colors on page
-function renderColors(colors) {
-  colorSchemeEl.innerHTML = colors
-    .map(
-      ({ hex, name }, index) => `
-    <section class="color">
-    <div class="color-bar" style="background-color:${hex.value}"></div>
-    <div class="hex" data-hex="${index}">${hex.value}</div>
-    </section>
-    `
-    )
-    .join("");
-
-  // If saved color schemes are found in local storage they are rendered on the page
-  if (localStorage.getItem("savedSchemes")) {
-    retrievedColors = JSON.parse(localStorage.getItem("savedSchemes"));
-    renderSavedSchemes(retrievedColors);
-  }
-}
-
-// Fetches default color scheme on page load
-fetchColorScheme(selectedColor, selectedScheme, selectedNumber);
